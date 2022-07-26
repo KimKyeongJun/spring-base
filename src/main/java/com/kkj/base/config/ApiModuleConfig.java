ff9b61d5e47531cacba85e5c78980346d54f21f2
Kyeong Jun <v49011591@gmail.com>
2022-07-26 16:05:11 +0900

    Update ApiModuleConfig.java

--- src/main/java/com/kkj/base/config/ApiModuleConfig.java
+++ src/main/java/com/kkj/base/config/ApiModuleConfig.java
@@ -19,6 +19,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
+
+// ThirdParty API 호출을 위한 모듈 Bean Container에 추가
 @Configuration
 @Slf4j
 public class ApiModuleConfig {
